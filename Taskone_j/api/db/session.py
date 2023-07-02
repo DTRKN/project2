61c00c2130b587fb0dcf1984f8ecb3d0cba0aad0
DTRKN <taratorkin.den@mail.ru>
2023-07-02 13:11:23 +0300

    session

--- Taskone_j/api/db/session.py
+++ Taskone_j/api/db/session.py
@@ -12,6 +12,6 @@ db_name = os.environ.get('POSTGRES_DB')
 db_host = os.environ.get('DB_HOST')
 
 #app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{db_user}:{db_password}@{db_host}:5432/{db_name}"
-app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://postgres:tarawater@localhost:5432/project2"
+app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://postgres:(...)@localhost:5432/project2"
 db = SQLAlchemy(app)
 engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
